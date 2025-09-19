97062c45b0c12cdc4f45e50095c8c60b494c52ea
mathaillah <mathaillah96@gmail.com>
2025-09-19 21:56:36 +0700

    belajar next.js video no.2 19/09/2025

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-
-import { Inter } from 'next/font/google'
-
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from '@/components/layouts/Navbar'
 
 export default function Home() {
   return (
     <div>
+    
       <div>Hello World</div>
     </div>
-  )
-}
+  );
+};
