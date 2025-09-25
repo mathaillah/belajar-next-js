2632881338e1a65c0711321c6f4a174c57c1cc57
mathaillah <mathaillah96@gmail.com>
2025-09-25 08:16:13 +0700

    next js explore 25/09/25

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import AppShell from '@/components/layouts/AppShell'
-import Navbar from '@/components/layouts/Navbar'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import AppShell from '@/components/layouts/AppShell';
+// import Navbar from '@/components/layouts/Navbar';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
    return (
---
+++ src/pages/shop/[...slug].tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+
+const ShopPage = () => {
+    const { query } = useRouter();
+
+    return (
+        <div> 
+            <h1>Shop Page</h1>
+            
+        </div>
+    );
+};
+
+export default ShopPage;
