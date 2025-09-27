9aef13c9670891b6fc8f394985cd8fcfdf70138d
mathaillah <mathaillah96@gmail.com>
2025-09-27 23:04:55 +0700

    next.js 27/09/25)

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,9 +1,22 @@
-const ProductSettingPage = () => {
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+
+const ProductPage = () => {
+    const[isLogin, setIsLogin] = useState(false);
+    const { push } = useRouter();
+    useEffect(() => {
+        if(!isLogin) {
+            push("/auth/login");
+        }
+    }, []);
     return (
         <div>
              <h1>Product Page</h1>
         </div>
     );
+    
 };
 
-export default ProductSettingPage;
+export default ProductPage;
