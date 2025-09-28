d22a12ef7f0cc6207adae6108815a77c1d5552cf
mathaillah <mathaillah96@gmail.com>
2025-09-28 11:59:47 +0700

    next js video 4 28/09/25

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -6,11 +6,13 @@ import { useEffect, useState } from "react";
 const ProductPage = () => {
     const[isLogin, setIsLogin] = useState(false);
     const { push } = useRouter();
-    useEffect(() => {
-        if(!isLogin) {
-            push("/auth/login");
-        }
-    }, []);
+
+    // useEffect(() => {
+    //     if(!isLogin) {
+    //         push("/auth/login");
+    //     }
+    // }, []);
+    
     return (
         <div>
              <h1>Product Page</h1>
