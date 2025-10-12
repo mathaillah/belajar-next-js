d6914b9ec6aa1082c3f74ddf402fcdef147c7d34
mathaillah <43997880+mathaillah@users.noreply.github.com>
2025-10-13 06:23:07 +0700

    Update [product].tsx

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -5,7 +5,7 @@ const DetailProductPage = () => {
 
     return (
         <div> 
-            <h1>Detail Product</h1>
+            <h1>Detail Product Page</h1>
             <p> Product : {query.product}</p>
         </div>
     );
